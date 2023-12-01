f98496ba57e3e537d737fd95e22e2b12033a0aae
dev-kudli <sudarshan97.kudli@gmail.com>
2023-12-01 15:41:58 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-85eda50c7bd4707e36d4.js"
+    "url": "webpack-runtime-4289f3ed739390a7e626.js"
   },
   {
     "url": "framework-025a10830d88a05b2804.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1fa9458924278f31ba2d8205b96fc390"
+    "revision": "65731242689ad9e906b774a5be2843a0"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-9b1997f2483a747a5203.js"
